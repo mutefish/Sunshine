53f5d6a519f9ea0a7fee75b33abb62c53518f757
mutefish <karenho008@gmail.com>
2016-06-30 19:30:45 -0400

    Completed lessons 1, 2 and 3

--- app/src/androidTest/java/com/example/kinlho/sunshine/app/ApplicationTest.java
+++ app/src/androidTest/java/com/example/kinlho/sunshine/app/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.example.android.sunshine.app;
+package com.example.kinlho.sunshine.app;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- app/src/main/java/com/example/kinlho/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/kinlho/sunshine/app/DetailActivity.java
@@ -1,21 +1,22 @@
-package com.example.android.sunshine.app;
+package com.example.kinlho.sunshine.app;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-
-public class MainActivity extends ActionBarActivity {
+public class DetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_detail);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
@@ -23,10 +24,11 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.detail, menu);
         return true;
     }
 
@@ -56,7 +58,13 @@ public class MainActivity extends ActionBarActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            View rootView = inflater.inflate(R.layout.activity_detail, container, false);
+            Intent intent = getActivity().getIntent();
+            if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
+                String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+                ((TextView) rootView.findViewById(R.id.detail_text)).setText(forecastStr);
+            }
             return rootView;
         }
     }
