d5c713c56a0e856cb1e749ce84ba2762dc5eb235
mutefish <karenho008@gmail.com>
2016-07-01 14:24:43 -0400

    Created location table

--- app/src/main/java/com/example/kinlho/sunshine/app/data/WeatherContract.java
+++ app/src/main/java/com/example/kinlho/sunshine/app/data/WeatherContract.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.example.android.sunshine.app.data;
+package com.example.kinlho.sunshine.app.data;
 
 import android.provider.BaseColumns;
 import android.text.format.Time;
